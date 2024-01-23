70a590d5839ada290d69b16f94fba57e4b99f673
edwakuwaku <edwardong@realtek-sg.com>
2024-01-23 11:50:49 +0800

    Add wifi keep alive interrupt callback

--- os/arch/arm/src/amebasmart/amebasmart_pmhelpers.c
+++ os/arch/arm/src/amebasmart/amebasmart_pmhelpers.c
@@ -52,6 +52,66 @@
 static RTIM_TimeBaseInitTypeDef TIM_InitStruct_GT[8];
 extern struct timer_s g_timer_wakeup;
 
+void SOCPS_SetAPWakeEvent_MSK0(u32 Option, u32 NewStatus)
+{
+	u32 WakeEvent = 0;
+
+	/* Set Event */
+	WakeEvent = HAL_READ32(PMC_BASE, WAK_MASK0_AP);
+	if (NewStatus == ENABLE) {
+		WakeEvent |= Option;
+	} else {
+		WakeEvent &= ~Option;
+	}
+	HAL_WRITE32(PMC_BASE, WAK_MASK0_AP, WakeEvent);
+}
+
+/**
+  * @brief  set ap wake up event mask1.
+  * @param  Option:
+  *   This parameter can be any combination of the following values:
+  *		 @arg WAKE_SRC_XXX
+  * @param  NewStatus: TRUE/FALSE.
+  * @retval None
+  */
+void SOCPS_SetAPWakeEvent_MSK1(u32 Option, u32 NewStatus)
+{
+	u32 WakeEvent = 0;
+
+	/* Set Event */
+	WakeEvent = HAL_READ32(PMC_BASE, WAK_MASK1_AP);
+	if (NewStatus == ENABLE) {
+		WakeEvent |= Option;
+	} else {
+		WakeEvent &= ~Option;
+	}
+	HAL_WRITE32(PMC_BASE, WAK_MASK1_AP, WakeEvent);
+}
+
+/**
+  * @brief  get aon wake reason.
+  * @param  None
+  * @retval aon wake up reason
+  *		This parameter can be one or combination of the following values:
+  *		 @arg AON_BIT_RTC_ISR_EVT
+  *		 @arg AON_BIT_GPIO_PIN3_WAKDET_EVT
+  *		 @arg AON_BIT_GPIO_PIN2_WAKDET_EVT
+  *		 @arg AON_BIT_GPIO_PIN1_WAKDET_EVT
+  *		 @arg AON_BIT_GPIO_PIN0_WAKDET_EVT
+  *		 @arg AON_BIT_TIM_ISR_EVT
+  *		 @arg AON_BIT_BOR_ISR_EVT
+  *		 @arg AON_BIT_CHIPEN_LP_ISR_EVT
+  *		 @arg AON_BIT_CHIPEN_SP_ISR_EVT
+  */
+int SOCPS_AONWakeReason(void)
+{
+	int reason = 0;
+
+	reason = HAL_READ32(SYSTEM_CTRL_BASE_LP, REG_AON_AON_WAK_EVT);
+
+	return reason;
+}
+
 void pg_timer_int_handler(void *Data)
 {
 	pmvdbg("Timer wakeup interrupt handler!!\n");
@@ -93,5 +153,34 @@ void ap_timer_helper(void) {
 		up_set_timer_interrupt(1, g_timer_wakeup.timer_interval);
 	}
 }
+
+/* Interrupt callback from wifi-keepalive, which LP received designated packet*/
+void SOCPS_LPWAP_ipc_int(VOID *Data, u32 IrqStatus, u32 ChanNum)
+{
+	/* To avoid gcc warnings */
+	UNUSED(Data);
+	UNUSED(IrqStatus);
+	UNUSED(ChanNum);
+
+	pmvdbg("IPC wakeup interrupt handler!!\n");
+	pm_activity(PM_IDLE_DOMAIN, 9);
+	ipc_get_message(IPC_LP_TO_AP, IPC_L2A_Channel1);
+
+}
+
+IPC_TABLE_DATA_SECTION
+
+const IPC_INIT_TABLE ipc_LPWHP_table[] = {
+	{
+		.USER_MSG_TYPE = IPC_USER_DATA,
+		.Rxfunc = SOCPS_LPWAP_ipc_int,
+		.RxIrqData = (VOID *) NULL,
+		.Txfunc = IPC_TXHandler,
+		.TxIrqData = (VOID *) NULL,
+		.IPC_Direction = IPC_LP_TO_AP,
+		.IPC_Channel = IPC_L2A_Channel1
+	},
+
+};
 
 #endif
